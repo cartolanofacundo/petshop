4b9132aad725717cc8682cd28b1f18becd12ec9d
Joaquin Dimaro <joacodimaro97@gmail.com>
2023-04-17 10:13:57 -0300

    formulario final

--- assets/app/contact.js
+++ assets/app/contact.js
@@ -11,14 +11,6 @@ $form.addEventListener('submit', (e)=>{
     $form.reset()
 })
 
- 
-Swal.fire({
-    title: 'Sweet!',
-    text: 'Modal with a custom image.',
-    imageUrl: 'https://unsplash.it/400/200',
-    imageWidth: 400,
-    imageHeight: 200,
-    imageAlt: 'Custom image',
-  })
+
 
 
